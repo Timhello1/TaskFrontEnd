84a6604341bb4a9d6d4fdcb67859fe48aed301e5
Timhello1 <calkatym@gmail.com>
2024-03-07 02:42:56 +0100

    final commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {RegisterComponent} from "./register/register.component";
+import {LoginComponent} from "./login/login.component";
+import {BeforeLoginService} from "./services/before-login.service";
+import {AfterLoginService} from "./services/after-login.service";
+import {TaskEditComponent} from "./task-edit/task-edit.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent, canActivate : [AfterLoginService]},
+  {path: 'Login', component: LoginComponent, canActivate : [BeforeLoginService]},
+  {path: 'Register', component: RegisterComponent, canActivate : [BeforeLoginService]},
+  {path: 'edit/:id', component: TaskEditComponent, canActivate : [AfterLoginService]}
+];
---
+++ src/app/models.ts
@@ -0,0 +1,8 @@
+export interface TaskResponse{
+  status: number;
+  tasks: any[];
+}
+export interface EditResponse{
+  status: number;
+  task: any[];
+}
